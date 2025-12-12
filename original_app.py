41585e8bcd788d3b63efab4cf2c10bdbd0729b45
DavidLopezGarci4 <65856067+DavidLopezGarci4@users.noreply.github.com>
2025-12-13 00:59:39 +0100

    Add files via upload
    disfraz de "Soy el robot de Google indexando tu web"

--- original_app.py
+++ original_app.py
@@ -25,11 +25,11 @@ st.set_page_config(page_title="Rastreador Master MOTU", page_icon="⚔️", layo
 # --- UTILIDADES DE NORMALIZACIÓN (NUEVO) ---
 import requests
 
-# --- CONFIGURACIÓN DE NAVEGADOR ESTÁTICO (HEADERS PRO) ---
+# --- CONFIGURACIÓN DE NAVEGADOR ESTÁTICO (MODO GOOGLEBOT) ---
 HEADERS_STATIC = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5X Build/MMB29P) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Mobile Safari/537.36 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
-    "Accept-Language": "es-ES,es;q=0.9,en;q=0.8",
+    "Accept-Encoding": "gzip, deflate, br",
     "Referer": "https://www.google.com/"
 }
 
