d7b10f5ef6cbaaa701483a876a9d31bbb8a71fd8
DavidLopezGarci4 <65856067+DavidLopezGarci4@users.noreply.github.com>
2025-12-12 23:54:12 +0100

    Add files via upload
    Eliminada línea que permitía compresión brotli

--- original_app.py
+++ original_app.py
@@ -30,8 +30,6 @@ HEADERS_STATIC = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8",
     "Accept-Language": "es-ES,es;q=0.9,en;q=0.8",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Connection": "keep-alive",
     "Referer": "https://www.google.com/"
 }
 
