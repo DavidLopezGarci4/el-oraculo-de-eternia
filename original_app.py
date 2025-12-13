e70e9bef7c71bf1a3510557ea62378357f9a808f
DavidLopezGarci4 <65856067+DavidLopezGarci4@users.noreply.github.com>
2025-12-13 13:50:23 +0100

    Add files via upload
    Modificado original_app.py

--- original_app.py
+++ original_app.py
@@ -4,6 +4,7 @@ import asyncio
 # from playwright.async_api import async_playwright # REMOVED
 from bs4 import BeautifulSoup
 import os
+from logger import log_structured
 # import subprocess # REMOVED
 # import sys # REMOVED
 
